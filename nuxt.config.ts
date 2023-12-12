c38758b4aa267030b26bb9f365a699a21bf7869f
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-12-12 11:26:37 +0100

    added cart persistence after refresh

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@nuxtjs/supabase",
     "@vueuse/nuxt",
+    "@pinia-plugin-persistedstate/nuxt",
   ],
   app: {
     pageTransition: { name: "page", mode: "out-in" },
