7c3bc0cd14c42303d7a738ddd88f3ce2c6a50f39
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-12-12 16:23:59 +0100

    added email login functionality

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
-    {{ data }}
     <div v-if="user">
-      {{ user.user_metadata.name }}
-      {{ user.user_metadata.picture }}
+      <!-- {{ user.user_metadata.name }}
+      {{ user.user_metadata.picture }} -->
+      {{ user }}
     </div>
     <h1>Login</h1>
-    <button @click="login">Login with Google</button>
+    <button @click="googleLogin">Login with Google</button>
+    <div>
+      <h1>Sign Up with email</h1>
+      <input type="email" v-model="email" placeholder="email" />
+      <input type="text" v-model="password" placeholder="password" />
+      <input type="text" v-model="name" placeholder="Name" />
+      <input type="checkbox" v-model="isAdmin" />
+      <button @click="signUpWithEmail">Login with Email</button>
+      <p v-if="errorMsg">{{ errorMsg }}</p>
+    </div>
+    <div>
+      <h1>Login with email</h1>
+      <input type="email" v-model="email" placeholder="email" />
+      <input type="text" v-model="password" placeholder="password" />
+      <!-- <input type="text" v-model="name" placeholder="Name" /> -->
+      <input type="checkbox" v-model="isAdmin" />
+      <button @click="loginWithEmail">Login with Email</button>
+      <p v-if="errorMsg">{{ errorMsg }}</p>
+    </div>
   </div>
 </template>
 
@@ -14,13 +32,18 @@
 useHead({
   title: "Login",
 });
+
+const email = ref("");
+const password = ref("");
+const name = ref("");
+const isAdmin = ref(false);
+const errorMsg = ref("");
+
 const supabase = useSupabaseClient();
 
 const user = useSupabaseUser();
 
-const data = ref([]);
-
-async function login() {
+async function googleLogin() {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
   });
@@ -29,14 +52,53 @@ async function login() {
     console.log(error);
     return;
   }
-
-  // const { data, error: fetchError } = await supabase
-  //   .from("users")
-  //   .select("isAdmin")
-  //   .eq("id", user.id);
-
-  // this.data = data;
-
-  // console.log(data);
 }
+
+const signUpWithEmail = async () => {
+  try {
+    const { error } = await supabase.auth.signUp(
+      {
+        email: email.value,
+        password: password.value,
+        isAdmin: isAdmin.value,
+      },
+      {
+        data: {
+          first_name: name.value,
+        },
+      }
+    );
+    email.value = "";
+    password.value = "";
+    if (error) throw error;
+    navigateTo("/");
+  } catch (error) {
+    errorMsg.value = error.message;
+  }
+};
+
+const loginWithEmail = async () => {
+  try {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: email.value,
+      password: password.value,
+      // name: name.value,
+    });
+    email.value = "";
+    password.value = "";
+    if (error) throw error;
+    console.log(data);
+    navigateTo("/");
+  } catch (error) {
+    errorMsg.value = error.message;
+  }
+};
+
+// watchEffect(() => {
+//   if (user.value) {
+//     return navigateTo('/');
+//   } }
+
+//britta.meyerling@gmail.com
+// 123456
 </script>
