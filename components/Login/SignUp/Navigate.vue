0428a36ad8892505ac193aeede974f258c4fecc3
Rahul Suvarna <rahulsuvarna18@gmail.com>
2023-12-21 18:34:29 +0100

    leaner login page

---
+++ components/Login/SignUp/Navigate.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="py-3 flex items-center justify-center gap-2">
+    <span>Not a registered user? Click</span>
+    <button
+      @click="navigateTo('/signUp')"
+      class="rounded-md bg-blue-600 px-8 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
+    >
+      Here
+    </button>
+  </div>
+</template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,84 +1,7 @@
 <template>
   <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
     <LoginSignInText text="Sign In" />
-
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <div class="space-y-6">
-        <div>
-          <label
-            for="email"
-            class="block text-sm font-medium leading-6 text-gray-900"
-            >Email address</label
-          >
-          <div class="mt-2">
-            <input
-              v-model="email"
-              id="email"
-              name="email"
-              type="email"
-              autocomplete="email"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-
-        <div>
-          <div class="flex items-center justify-between">
-            <label
-              for="password"
-              class="block text-sm font-medium leading-6 text-gray-900"
-              >Password</label
-            >
-          </div>
-          <div class="mt-2">
-            <input
-              v-model="password"
-              id="password"
-              name="password"
-              type="password"
-              autocomplete="current-password"
-              required
-              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            />
-          </div>
-        </div>
-
-        <div>
-          <button
-            @click="loginWithEmail"
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Sign in
-          </button>
-        </div>
-      </div>
-      <div class="py-3 flex items-center justify-center gap-2">
-        <span>Not a registered user? Click</span>
-        <button
-          @click="navigateTo('/signUp')"
-          class="rounded-md bg-blue-600 px-8 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-400"
-        >
-          Here
-        </button>
-      </div>
-
-      <p class="mt-10 text-center text-sm text-gray-500">
-        <a
-          href="#"
-          class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-          >Or continue with</a
-        >
-        <button
-          @click="googleLogin"
-          type="submit"
-          class="flex w-full justify-center rounded-md bg-rose-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-400"
-        >
-          Google
-        </button>
-      </p>
-    </div>
+    <LoginBody />
   </div>
 </template>
 
@@ -86,36 +9,4 @@
 useHead({
   title: "Login",
 });
-
-const email = ref<string>("");
-const password = ref<string>("");
-const errorMsg = ref<string>("");
-
-const supabase = useSupabaseClient();
-
-async function googleLogin() {
-  const { error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-  });
-
-  if (error) {
-    console.log(error);
-    return;
-  }
-}
-
-const loginWithEmail = async () => {
-  try {
-    const { data, error } = await supabase.auth.signInWithPassword({
-      email: email.value,
-      password: password.value,
-    });
-    email.value = "";
-    password.value = "";
-    if (error) throw error;
-    navigateTo("/");
-  } catch (error: any) {
-    errorMsg.value = error.message;
-  }
-};
 </script>
