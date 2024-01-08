5688e25ef886c4dcd145a2d79cfe8ea4c3bf181b
Rahul Suvarna <rahulsuvarna18@gmail.com>
2024-01-08 16:31:23 +0100

    added vuetify to the sign up page and components

--- components/Login/SignUp/Email.vue
+++ components/Login/SignUp/Email.vue
@@ -1,55 +1,36 @@
 <template>
-  <div class="space-y-6">
-    <div>
-      <label
-        for="email"
-        class="block text-sm font-medium leading-6 text-gray-900"
-        >Email address</label
-      >
-      <div class="mt-2">
-        <input
-          v-model="email"
-          id="email"
-          name="email"
-          type="email"
-          autocomplete="email"
-          required
-          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
-      </div>
-    </div>
+  <v-container class="space-y-6">
+    <v-row>
+      <v-col>
+        <v-label for="email" text="Email Address"></v-label>
+        <v-text-field v-model="email" rounded outlined></v-text-field>
+      </v-col>
+    </v-row>
 
-    <div>
-      <div class="flex items-center justify-between">
-        <label
-          for="password"
-          class="block text-sm font-medium leading-6 text-gray-900"
-          >Password</label
-        >
-      </div>
-      <div class="mt-2">
-        <input
+    <v-row>
+      <v-col>
+        <v-label text="Password"></v-label>
+        <v-text-field
           v-model="password"
-          id="password"
-          name="password"
-          type="password"
-          autocomplete="current-password"
           required
-          class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-        />
-      </div>
-    </div>
+          rounded
+          outlined
+        ></v-text-field>
+      </v-col>
+    </v-row>
 
-    <div>
-      <button
-        @click="signUpWithEmail"
-        type="submit"
-        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      >
-        Sign up
-      </button>
-    </div>
-  </div>
+    <v-row>
+      <v-col>
+        <v-btn
+          @click="signUpWithEmail"
+          color="indigo"
+          rounded
+          text="Sign up"
+          width="8rem"
+        ></v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script setup lang="ts">
