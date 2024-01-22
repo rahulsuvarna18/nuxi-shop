c4990a1fea82828b88ea8208e130d461f4747b9c
Rahul Suvarna <rahulsuvarna18@gmail.com>
2024-01-22 18:45:20 +0100

    added typescript functionality to cart.vue page

--- pages/cart.vue
+++ pages/cart.vue
@@ -3,7 +3,11 @@
     v-if="cartStore.cartInfo.length"
     class="d-flex ga-3 flex-col align-start justify-center w-25"
   >
-    <CartItems v-for="item in cartStore.cartInfo" :key="item.id" :item="item" />
+    <CartItems
+      v-for="(cartItem, i) in cartStore.cartInfo"
+      :key="i"
+      :item="cartItem as CartItem"
+    />
     <CartTotal :total="cartStore.total" />
   </v-container>
   <v-container class="d-flex flex-col align-center justify-center ga-2" v-else>
@@ -13,6 +17,14 @@
 
 <script setup lang="ts">
 import { useCartStore } from "@/stores/cart";
+
+type CartItem = {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+  totalPrice: number;
+};
 
 useHead({
   title: "Ice Cream Cart",
